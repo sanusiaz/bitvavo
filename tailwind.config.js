bb59944be7a965fc605f8b966ae2983df2d3928c
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2022-08-27 08:47:52 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'btn': '#0051ff',
+        'btn-hover': '#002fa7',
+        'az-light': '#8594a1',
+        'az-dark': '#2f3a45',
+        'section-bg': '#fafcff',
+        'check': 'rgb(235, 244, 255)'
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'outfit': ['Poppins', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+        'gilroy': ['Gilroy', 'sans-serif'],
+      },
+      screens: {
+        'smm': '300px',
+        'mdd': '500px'
+      }
+    },
+  },
+  plugins: [],
+}
